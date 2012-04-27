43cc95dcbd52775b275e014a3e6325da6a46ba46
Kieran McGrady <kieran@hotrodsoftware.com>
2012-04-27 14:32:32 -0400

    Removed unused code

--- Reader/WebViewController.m
+++ Reader/WebViewController.m
@@ -12,15 +12,6 @@
 @implementation WebViewController
 @synthesize url = _url, webView = _webView;
 
-//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil url:(NSURL *)postURL title:(NSString *)postTitle
-//{
-//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-//    if (self) {
-//        // Custom initialization
-//    }
-//    return self;
-//}
-
 - (id)initWithURL:(NSURL *)postURL title:(NSString *)postTitle
 {
     self = [super init];
@@ -34,13 +25,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    
     _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [self.view addSubview:self.webView];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    NSLog(@"URL: %@", self.url);
     [self.webView loadRequest:[NSURLRequest requestWithURL:self.url]];
 }
 
